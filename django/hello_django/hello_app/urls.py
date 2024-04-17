b0744a3afa7bbaae7ef9c992567b6a3303f01953
yedhukrishnaG <yedhukrishna.girish@implify.de>
2024-04-17 17:38:55 +0200

    test

--- django/hello_django/hello_app/urls.py
+++ django/hello_django/hello_app/urls.py
@@ -30,7 +30,7 @@ urlpatterns = [
     path('homepage/', views.homepage, name='homepage'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('addrow/', views.addrow, name='addrow'),
-    path('login/', views.login, name='login'),
     path('signup/', views.signup, name='signup'),
+    path('login/', views.login, name='login'),
     path('', views.print_hello),
 ]
